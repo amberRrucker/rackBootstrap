6bd1ea3e7b3bb3223098cae830552d674abef82d
akbowlin <akbowlin@gmail.com>
2015-11-18 02:32:41 -0600

    adjusted icons for topNav

--- app/partials/topNav/topNavCtrl.js
+++ app/partials/topNav/topNavCtrl.js
@@ -2,7 +2,19 @@
     var topNavCtrl = function ($scope) {
         $scope.navigation = ["Why Rackspace", "Dedicated Hosting", "Cloud", "Infrastructure", "Solutions", "Support"];
 
+        $('.navbar').on('show.bs.collapse', function () {
+            var actives = $(this).find('.collapse.in'),
+                hasData;
+
+            if (actives && actives.length) {
+                hasData = actives.data('collapse');
+                if (hasData && hasData.transitioning) return;
+                actives.collapse('hide');
+                hasData || actives.data('collapse', null)
+            }
+        });
 
     };
     angular.module('rackBoot').controller("topNavCtrl", ["$scope", topNavCtrl]);
 })();
+
